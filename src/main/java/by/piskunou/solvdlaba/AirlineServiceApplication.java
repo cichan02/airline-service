f3f09d0a1220eaea6a290ba827424604c3aaecc0
cpiskunou solvd <cpiskunou@solvd.com>
2023-03-31 12:04:55 +0300

    init

---
+++ src/main/java/by/piskunou/solvdlaba/AirlineServiceApplication.java
@@ -0,0 +1,13 @@
+package by.piskunou.solvdlaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineServiceApplication.class, args);
+	}
+
+}
